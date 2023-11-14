c69448c2b3c134743b3f57daebaadfe8e0f22d37
RufatAzizov <mi6wkrck6@code.edu.az>
2023-11-14 13:07:50 +0400

    first

---
+++ ConsoleApp1900/ConsoleApp1900/CustomObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1900
+{
+    public class CustomObject
+    {
+        public uint userId;
+        public uint id;
+        public string title;
+        public string body;
+        
+    }
+}
---
+++ ConsoleApp1900/ConsoleApp1900/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+namespace ConsoleApp1900
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string modelsDirectory = @"c:\users\birinci novbe\desktop\consoleapp1900\consoleapp1900\Models";
+            string dataDirectory = @"c:\users\birinci novbe\desktop\consoleapp1900\consoleapp1900\Data";
+            string file = "jsonData.json";
+            string fileDirectory = @"c:\users\birinci novbe\desktop\consoleapp1900\consoleapp1900\Data\jsonData.json";
+            List <CustomObject> CustomObjects = new List<CustomObject>();
+
+            if (!Directory.Exists(modelsDirectory) || !Directory.Exists(dataDirectory))
+            {
+                Directory.CreateDirectory(modelsDirectory);
+                Directory.CreateDirectory(dataDirectory);
+            }
+            if (!File.Exists(Path.Combine(dataDirectory, fileDirectory)));
+            {
+                File.Create(fileDirectory);
+                //using (File.Create(fileDirectory));
+            }
+
+            HttpClient client = new();
+            string response = await client.GetStringAsync(@"https://jsonplaceholder.typicode.com/posts");
+            CustomObjects = JsonConvert.DeserializeObject<List<CustomObject>>(response);
+            using (StreamWriter writer = new(fileDirectory))
+            {
+                writer.Write(JsonConvert.SerializeObject(CustomObjects));
+            }
+            
+        }
+    }
+}
