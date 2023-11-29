3886b0109f11684bed0ae9aa88469ee783993ad6
RufatAzizov <mi6wkrck6@code.edu.az>
2023-11-29 13:17:07 +0400

    commit

---
+++ FirstWebApplication/FirstWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using FirstWebApplication.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace FirstWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var dt = await SqlHelper.GetDatas("SELECT * FROM Authors");
+            return View(dt);
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+    }
+    
+    
+}
---
+++ FirstWebApplication/FirstWebApplication/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using FirstWebApplication.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApplication.Controllers
+{
+    //public class LoginController : Controller
+    //{
+    //    public async Task<IActionResult> Login()
+    //    {
+    //        var dt = await SqlHelper.GetDatas("SELECT * FROM Authors");
+    //        return View(dt);
+    //    }
+    //    public IActionResult Contact()
+    //    {
+    //        return View();
+    //    }
+    //}
+}
