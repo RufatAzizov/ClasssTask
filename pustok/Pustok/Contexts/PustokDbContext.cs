48f38ce02148503c634fe1dd354c2101020c3b27
RufatAzizov <mi6wkrck6@code.edu.az>
2023-11-30 13:39:08 +0400

    Commit

---
+++ pustok/Pustok/Contexts/PustokDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pustok.Models;
+namespace Pustok.Contexts
+{
+    public class PustokDbContext : DbContext
+    {
+        public DbSet<Slider> Sliders { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = DESKTOP-U9AO5GJ\\SQLEXPRESS; Database = Pustok; Trusted_Connection = true");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ pustok/Pustok/Models/Slider.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pustok.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(128)]
+        public string ImageUrl { get; set; }
+        [Required, MinLength(3), MaxLength(64), DataType("nvarchar")]
+        public string Title { get; set; }
+        [Required, MinLength(3), MaxLength(128), DataType("nvarchar")]
+
+        public string Text { get; set; }
+        public bool IsLeft { get; set; }
+    }
+}
