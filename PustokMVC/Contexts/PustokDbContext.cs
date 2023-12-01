94ec4b3d1a207e1814bb6bedbf01798913e451cd
RufatAzizov <mi6wkrck6@code.edu.az>
2023-12-01 13:14:45 +0400

    Commit

---
+++ PustokMVC/Contexts/PustokDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PustokMVC.Models;
+
+namespace PustokMVC.Contexts
+{
+    public class PustokDbContext : DbContext
+    {
+        public DbSet<HeroSlider> HeroSliders { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-U9AO5GJ\\SQLEXPRESS;Database=PustokDB;Trusted_Connection=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ PustokMVC/Models/HeroSlider.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PustokMVC.Models
+{
+    public class HeroSlider
+    {
+        public int Id { get; set; }
+        [Required, MinLength(3), MaxLength(10)]
+        public string Title { get; set; }
+        [Required, MinLength(3), MaxLength(30)]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required, MinLength(3), MaxLength(255)]
+        public string ImageURL { get; set; }
+        public bool IsRight { get; set; }
+    }
+}
